9ea503c43e3f0cfc4651abf57c218859d429b132
Aleck Tuluenga <aleckbryson@gmail.com>
2020-08-06 19:11:18 -0400

    Start button modified to hide when clicked

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function startButton() {
+    var x = document.getElementById("start-button");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
